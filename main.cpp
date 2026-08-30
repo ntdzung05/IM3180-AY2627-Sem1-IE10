0ee070df0b786f78def647ada9b09b2063cc7617
Karan9246 <78999574+Karan9246@users.noreply.github.com>
2026-08-30 14:38:03 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
-#include"board.hpp"
-#include<iostream>
+#include "UI files/ui.hpp"
 
-int main(){
-
-    Board game;
-
-    while(!game.check_game_ended()){
-        int old_x, old_y, new_x, new_y;
-        game.get_turn();
-        for(auto& i: game){
-            for(auto& j: i){
-                std::cout << j << ' ';
-            }
-            std::cout << '\n';
-        }
-        std::cin >> old_x >> old_y >> new_x >> new_y;
-        game.make_move(old_x, old_y, new_x, new_y);
-        std::cout << game.board_eval() << '\n';
-    }
+int main() {
+    ChessUI ui;
+    ui.run();
 
     return 0;
 }
